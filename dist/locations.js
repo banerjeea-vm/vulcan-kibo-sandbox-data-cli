41a9f8941305bf7e28f44245cbf98e3d7507560b
A Ganesh Kumar <kumarg@vmcmail.com>
2025-09-17 13:57:39 +0530

    ECM-58
    ECM-58

--- dist/locations.js
+++ dist/locations.js
@@ -2,9 +2,10 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+//Start:ECM-58
 const fs = require("fs");
 const path = require("path");
-const { parse } = require("csv-parse/sync");
+const { parse } = require("csv-parse/sync");//End:ECM-58
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteAllLocations = exports.importAllLocations = exports.exportAllLocations = void 0;
@@ -41,7 +42,8 @@ const generateLocation = async (locationData) => {
             try {
                 await locationTypeMethods.addLocationType(locationType);
             }
-            catch (error) {console.log('error:::--'+error); }
+            catch (error) { }
+
         }
     }
     try {
@@ -49,13 +51,13 @@ const generateLocation = async (locationData) => {
         console.log('Successfully added location');
     }
     catch (error) {
-        console.error('Error in adding statusCode', error.originalError.statusCode);
+        console.error(locationData.code+ 'Error in adding statusCode', error.originalError.statusCode);
         console.error('errorCode', error.originalError.errorCode);
         if (error.originalError.statusCode === 409 && error.originalError.errorCode ==='ITEM_ALREADY_EXISTS') {
             try {
-            console.log('trying to Update');
+            console.log(locationData.code+' is trying to Update');
                 await locationMethods.updateLocation({ locationCode: locationData.code }, { body: locationData });
-                console.log('Updated location Successfully');
+                console.log(locationData.code+' is Updated location Successfully');
             }
             catch (updateError) {
             console.log('=========error==========');
@@ -102,7 +104,7 @@ async function exportAllLocations() {
 }
 exports.exportAllLocations = exportAllLocations;
 async function importAllLocations() {
-convertCsvToJson();
+convertCsvToJson();//ECM-58
     initClients();
 
     let dataStream = (0, utilites_1.createJsonLFileStream)(dataFilePath);
@@ -127,10 +129,11 @@ async function deleteAllLocations() {
 }
 exports.deleteAllLocations = deleteAllLocations;
 
-  function convertCsvToJson(){
-// Input CSV file
+//ECM-58
+function convertCsvToJson(){
+
 const csvFile = path.resolve(__dirname, "../locations.csv");
-// Output JSON file (expected by kibo-sandbox-data-cli)
+
 const outputDir = path.resolve(__dirname, "../data");
 const outputFile = path.join(outputDir, "locations.jsonl");
 
@@ -175,76 +178,76 @@ const locations = records.map((r) => ({
   },
   fulfillmentTypes: [
     		{
-    			code: "DS",
-    			name: "Direct Ship"
-    		},
-    		{
     			code: "SP",
     			name: "In Store Pickup"
-    		}
+    		},
+    		{
+               code: "DL",
+               name: "Delivery"
+            },
   ],
  regularHours: {
                 sunday: {
                     label:r.Sunday ,
-                    openTime: ((r.Sunday && typeof r.Sunday === 'string' && r.Sunday.includes('-')) ? `${r.Sunday.split("-")[0].replace("AM", "")}` : null),
+                    openTime: ((r.Sunday && typeof r.Sunday === 'string' && r.Sunday.includes('-')) ? `${convertTo24Hour(r.Sunday.split("-")[0])}` : null),
                     closeTime:( (r.Sunday && typeof r.Sunday === 'string' && r.Sunday.includes('-')) ? `${convertTo24Hour(r.Sunday.split("-")[1])}` : null),
                     isClosed:  (r.Sunday && typeof r.Sunday === 'string' ) ? false : true
                 },
                 monday: {
                     label: r.Monday,
-                     openTime:( (r.Monday && typeof r.Monday === 'string' && r.Monday.includes('-')) ? `${r.Monday.split("-")[0].replace("AM", "")}` : null),
+                     openTime:( (r.Monday && typeof r.Monday === 'string' && r.Monday.includes('-')) ? `${convertTo24Hour(r.Monday.split("-")[0])}` : null),
                       closeTime:( (r.Monday && typeof r.Monday === 'string' && r.Monday.includes('-')) ? `${convertTo24Hour(r.Monday.split("-")[1])}` : null),
                        isClosed:  (r.Monday && typeof r.Monday === 'string' ) ? false : true
 
                 },
                  tuesday: {
                       label: r.Tuesday,
-                      openTime: ((r.Tuesday && typeof r.Tuesday === 'string' && r.Tuesday.includes('-')) ? `${r.Tuesday.split("-")[0].replace("AM", "")}` : null),
+                      openTime: ((r.Tuesday && typeof r.Tuesday === 'string' && r.Tuesday.includes('-')) ? `${convertTo24Hour(r.Tuesday.split("-")[0])}` : null),
                       closeTime: ((r.Tuesday && typeof r.Tuesday === 'string' && r.Tuesday.includes('-')) ? `${convertTo24Hour(r.Tuesday.split("-")[1])}` : null),
                       isClosed: (r.Tuesday && typeof r.Tuesday === 'string') ? false : true
                     },
                     wednesday: {
                       label: r.Wednesday,
-                      openTime: ((r.Wednesday && typeof r.Wednesday === 'string' && r.Wednesday.includes('-')) ? `${r.Wednesday.split("-")[0].replace("AM", "")}` : null),
+                      openTime: ((r.Wednesday && typeof r.Wednesday === 'string' && r.Wednesday.includes('-')) ? `${convertTo24Hour(r.Wednesday.split("-")[0])}` : null),
                       closeTime: ((r.Wednesday && typeof r.Wednesday === 'string' && r.Wednesday.includes('-')) ? `${convertTo24Hour(r.Wednesday.split("-")[1])}` : null),
                       isClosed: (r.Wednesday && typeof r.Wednesday === 'string') ? false : true
                     },
                     thursday: {
                       label: r.Thursday,
-                      openTime: ((r.Thursday && typeof r.Thursday === 'string' && r.Thursday.includes('-')) ? `${r.Thursday.split("-")[0].replace("AM", "")}` : null),
+                      openTime: ((r.Thursday && typeof r.Thursday === 'string' && r.Thursday.includes('-')) ? `${convertTo24Hour(r.Thursday.split("-")[0])}` : null),
                       closeTime: ((r.Thursday && typeof r.Thursday === 'string' && r.Thursday.includes('-')) ? `${convertTo24Hour(r.Thursday.split("-")[1])}` : null),
                       isClosed: (r.Thursday && typeof r.Thursday === 'string') ? false : true
                     },
                     friday: {
                       label: r.Friday,
-                      openTime: ((r.Friday && typeof r.Friday === 'string' && r.Friday.includes('-')) ? `${r.Friday.split("-")[0].replace("AM", "")}` : null),
+                      openTime: ((r.Friday && typeof r.Friday === 'string' && r.Friday.includes('-')) ? `${convertTo24Hour(r.Friday.split("-")[0])}` : null),
                       closeTime: ((r.Friday && typeof r.Friday === 'string' && r.Friday.includes('-')) ? `${convertTo24Hour(r.Friday.split("-")[1])}` : null),
                       isClosed: (r.Friday && typeof r.Friday === 'string') ? false : true
                     },
                     saturday: {
                       label: r.Saturday,
-                      openTime: ((r.Saturday && typeof r.Saturday === 'string' && r.Saturday.includes('-')) ? `${r.Saturday.split("-")[0].replace("AM", "")}` : null),
+                      openTime: ((r.Saturday && typeof r.Saturday === 'string' && r.Saturday.includes('-')) ? `${convertTo24Hour(r.Saturday.split("-")[0])}` : null),
                       closeTime: ((r.Saturday && typeof r.Saturday === 'string' && r.Saturday.includes('-')) ? `${convertTo24Hour(r.Saturday.split("-")[1])}` : null),
                       isClosed: (r.Saturday && typeof r.Saturday === 'string') ? false : true
                     },
                timeZone :r.TimeZone
             },
   shippingOriginContact: {
-  				firstName: r.ShippingContactFirstName,
+  				firstName: r.ShippingContactFirstName ,
   				middleNameOrInitial:"" ,
   				lastNameOrSurname: r.ShippingContactLastNameOrSurname,
                   companyOrOrganization: r.ShippingContactCompanyOrOrganization,
                   phoneNumber: r.ShippingContactPhoneNumber,
                   email: r.ShippingContactEmail
   },
-  supportsInventory: true,
-  allowFulfillmentWithNoStock: false,
-  isDisabled: false,
-  express: false,
-  transferEnabled: false,
-  includeInInventoryAggregrate: false,
-  includeInLocationExport : false,
-  warehouseEnabled: false
+  supportsInventory: r.SupportsInventory === 'Yes' ? true : false,
+  allowFulfillmentWithNoStock: r.AllowFulfillmentWithNoStock ==='Yes' ? true : false,
+  isDisabled: r.IsDisabled ==='Yes' ? true : false,
+  express: r.Express ==='Yes' ? true : false,
+  transferEnabled: r.TransferEnabled ==='Yes' ? true : false,
+  includeInInventoryAggregrate: r.IncludeInInventoryAggregrate ==='Yes' ? true : false,
+  includeInLocationExport : r.IncludeInLocationExport ==='Yes' ? true : false,
+  warehouseEnabled: r.WarehouseEnabled ==='Yes' ? true : false
 }));
 // Write JSON file
 const ndjson = locations.map((loc) => JSON.stringify(loc)).join("\n");
